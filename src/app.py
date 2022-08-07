f4cf19fd3b299548b6156b7debc634ca0e94b73b
marfurta <anamariamarfurt@gmail.com>
2022-08-07 01:08:58 +0000

    probar modelo en app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,71 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+import statsmodels.api as sm
+from sklearn.metrics import mean_squared_error, r2_score
+import pickle
 
-# your code here
+
+url='https://raw.githubusercontent.com/4GeeksAcademy/random-forest-project-tutorial/main/titanic_train.csv'
+df = pd.read_csv(url, index_col=[0,3])
+
+df=df.drop(columns='Cabin')
+
+df['Age']=df['Age'].fillna(df['Age'].mean())
+df['Embarked']=df['Embarked'].fillna(df['Embarked'].mode()[0])
+
+df[['Sex','Embarked','Survived']]=df[['Sex','Embarked','Survived']].astype('category')
+
+y=df['Survived']
+X=df.drop(columns=['Ticket','Survived']).copy()
+
+X['Sex']=X['Sex'].cat.codes
+X['Embarked']=X['Embarked'].cat.codes
+
+df[['Sex','Embarked','Survived']]=df[['Sex','Embarked','Survived']].astype('category')
+
+y=df['Survived']
+X=df.drop(columns=['Ticket','Survived']).copy()
+
+X['Sex']=X['Sex'].cat.codes
+
+X['Embarked']=X['Embarked'].cat.codes
+
+
+filename = '../models/finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+#Predict using the model
+Pclass = 3
+Sex = 1
+Age = 5.0
+SibSp = 2
+Parch = 1
+Ticket = 2666
+Fare = 19.2583
+#Cabin = NaN
+Embarked = 2
+
+#predigo el target para los valores seteados con modelo
+print('Predicted Survived 1 : \n', loaded_model.predict([[Fare,Age,Sex]]))
+
+Pclass = 3
+Sex = 0
+Age = 28.0
+SibSp = 0
+Parch = 0
+Ticket = 349207
+Fare = 7.8958
+#Cabin = NaN
+Embarked = 0
+
+
+#predigo el target para los valores seteados con modelo
+print('Predicted Survived 2: \n', loaded_model.predict([[Fare,Age,Sex]]))
+
+
